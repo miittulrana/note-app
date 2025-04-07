3d56059abd89c3e5688f81ba5151abd1641699f0
Mitul Rana <miittulrana@gmail.com>
2025-04-07 23:26:34 +0200

    GOT

--- src/features/common/utils/debounce.ts
+++ src/features/common/utils/debounce.ts
@@ -1,5 +1,5 @@
 export const debounce = (fn: (args: any) => void, delay = 300) => {
-  let timer: number | null = null;
+  let timer: ReturnType<typeof setTimeout> | null = null;
 
   return (arg: any) => {
     if (timer) clearTimeout(timer);
